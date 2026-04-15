c2eaa501fbdb58feac10cc7fd38a6dee2f7eacd6
toluasalu <johnasalu13@gmail.com>
2026-04-15 02:48:01 +0100

    Setup App Theme using NativeWind

---
+++ app/index.tsx
@@ -0,0 +1,17 @@
+import "@/global.css";
+import { Link } from "expo-router";
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-background">
+      <Text className="text-xl font-bold text-success">
+        Welcome to Nativewind!
+      </Text>
+      <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">Go to Onboarding</Link>
+      <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">Go to Sign In</Link>
+      <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">Go to Sign Up</Link>
+    </View>
+  );
+}
